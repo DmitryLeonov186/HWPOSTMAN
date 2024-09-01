9f61d0eca742bb060ef0f491e3e15d56b7f00f2e
Dmitry Leonov <lemomonster@mail.ru>
2024-09-01 23:20:37 +0500

    Inital commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanTest {
+
+    @Test
+    public void PostRawTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("API тест")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+
+    }
+}
