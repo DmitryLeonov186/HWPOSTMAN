b9a415cc6a520c65175d7aaf5e215dd7cbd1478b
Dmitry Leonov <lemomonster@mail.ru>
2024-09-01 23:40:45 +0500

    Fail test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -16,7 +16,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("API тест"));
+                .body("data", equalTo(100));
 
     }
 }
